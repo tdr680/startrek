28d4ac62113e6260122c7114587d4dd75e088180
Tomas <tomas@bebop>
2014-12-14 14:54:21 +0100

    Initial Commit

---
+++ StarTrek/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StarTrek
+//
+//  Created by Tomas on 14/12/14.
+//  Copyright (c) 2014 Tomas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
